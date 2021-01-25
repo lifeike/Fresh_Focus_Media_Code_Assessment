64c5f4b4d6ef2f305e0cd73ab937d99c01b363f3
lifeike <46255758+lifeike@users.noreply.github.com>
2021-01-24 23:35:09 -0700

    Update index.php

--- inventory/server/index.php
+++ inventory/server/index.php
@@ -28,6 +28,12 @@ if(isset($_GET["category"]) && isset($_GET["part"]) && isset($_GET["product"]) &
 	//create sql for please
 	$sql = "select {$please} from inventory where category='{$category}' and part='{$part}' and name='{$product}' group by {$please}";
 	$data = $pdo->query($sql)->fetchAll();
+	//1.给 app写接口时，要用 echo json_encode() 来返回数据，但是PHP自身调用却用 return json_encode();
+	//rerurn是语言中函数或者方法所代表的值，跟变量一样，储存在内存中。return是给其他代码块一个指针或者引用。
+	//而服务器与app（客户端）交互时，他们无法读取同一个内存系统，只能通过文本这样的方式来交换信息。
+	//所以要echo一段文本（json）给客户端。
+	//php惯用的输出流方式就是echo,如果你钟情于return输出，可以自创一门语言来实现。
+
 	echo json_encode($data);
 
 }else if(isset($_GET["category"])&& isset($_GET["part"])){
